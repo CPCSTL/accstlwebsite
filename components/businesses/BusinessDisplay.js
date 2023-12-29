3bcc85307a4f308525b83bb05d1212ffbcfe743e
CPCSTL <65110819+CPCSTL@users.noreply.github.com>
2023-12-28 20:30:20 -0600

    added display

--- components/businesses/BusinessDisplay.js
+++ components/businesses/BusinessDisplay.js
@@ -62,11 +62,11 @@ const BusinessCard = ({ business }) => (
 
 const CategoriesFilter = ({ onSelectCategory }) => (
     <Box sx={{ display: 'flex', flexDirection: 'row', marginBottom: 2, gap: 1 }}>
-        <Button variant="outlined" sx={{backgroundColor:"red"}} onClick={() => onSelectCategory('Middle Eastern Restaurant')}>Middle Eastern Restaurants</Button>
-        <Button variant="outlined" sx={{bgcolor:"blue"}} onClick={() => onSelectCategory('Immigration Attorney')}>Immigration Attorneys</Button>
-        <Button variant="outlined" sx={{bgcolor:"blue"}} onClick={() => onSelectCategory('Hair Saloon')}>Hair Saloons</Button>
-        <Button variant="outlined" sx={{bgcolor:"blue"}} onClick={() => onSelectCategory('Grocery Store')}>Grocery Stores</Button>
-        <Button variant="outlined" sx={{bgcolor:"blue"}} onClick={() => onSelectCategory('')}>Show All</Button>
+        <Button variant="outlined" onClick={() => onSelectCategory('Middle Eastern Restaurant')}>Middle Eastern Restaurants</Button>
+        <Button variant="outlined"  onClick={() => onSelectCategory('Immigration Attorney')}>Immigration Attorneys</Button>
+        <Button variant="outlined" onClick={() => onSelectCategory('Hair Saloon')}>Hair Saloons</Button>
+        <Button variant="outlined" onClick={() => onSelectCategory('Grocery Store')}>Grocery Stores</Button>
+        <Button variant="outlined" onClick={() => onSelectCategory('')}>Show All</Button>
     </Box>
 );
 
@@ -79,7 +79,7 @@ const BusinessList = () => {
 
     return (
         <Box sx={{
-            mt: 2,
+            mt: 9,
             width: '90%',
             ml: 'auto',
             mr: 'auto',
