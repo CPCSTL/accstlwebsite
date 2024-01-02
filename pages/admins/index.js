fbd8ff64d0cfd683cfa16f39e1bc9152246d84de
CPCSTL <65110819+CPCSTL@users.noreply.github.com>
2024-01-02 01:19:30 -0600

    moved out getserverside props

--- pages/admins/index.js
+++ pages/admins/index.js
@@ -168,34 +168,34 @@ const SignIn = (props)=>{
     )
 };
 
-export const getServerSideProps = async (context)=>{
-    //const session =  await getSession({req:context.req})
-    const req = context.req
-    let session 
-    try {
-     session = await getSession({req})
-    } catch (error) {
-      console.log(error);
-    }
-    console.log(session , "session at signin");
-    if(session){
-      return {
-          redirect:{
-              destination:"/admins/panel",
-              permanent:false
-          },
-          props:{
-            user:{session,reod:"user session"}
-          } 
-      }
-  }
-    return {
-        props:{
-            user:session
-        },
+// export const getServerSideProps = async (context)=>{
+//     //const session =  await getSession({req:context.req})
+//     const req = context.req
+//     let session 
+//     try {
+//      session = await getSession({req})
+//     } catch (error) {
+//       console.log(error);
+//     }
+//     console.log(session , "session at signin");
+//     if(session){
+//       return {
+//           redirect:{
+//               destination:"/admins/panel",
+//               permanent:false
+//           },
+//           props:{
+//             user:{session,reod:"user session"}
+//           } 
+//       }
+//   }
+//     return {
+//         props:{
+//             user:session
+//         }
         
-    }
-}
+//     }
+// }
 
 
 export default SignIn
