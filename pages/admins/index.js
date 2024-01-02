d70775ed22eb0d98cf906d4fdc35ddcfed83b2d2
CPCSTL <65110819+CPCSTL@users.noreply.github.com>
2024-01-02 03:42:35 -0600

    random logs

--- pages/admins/index.js
+++ pages/admins/index.js
@@ -172,26 +172,26 @@ export const getServerSideProps = async (context)=>{
     //const session =  await getSession({req:context.req})
     const req = context.req
     let session 
-    try {
-     session = await getSession({req})
-    } catch (error) {
-      console.log(error);
-    }
-    console.log(session , "session at signin");
-    if(session){
-      return {
-          redirect:{
-              destination:"/admins/panel",
-              permanent:false
-          },
-          props:{
-            user:{session,reod:"user session"}
-          } 
-      }
-  }
+  //   try {
+  //    session = await getSession({req})
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  //   console.log(session , "session at signin");
+  //   if(session){
+  //     return {
+  //         redirect:{
+  //             destination:"/admins/panel",
+  //             permanent:false
+  //         },
+  //         props:{
+  //           user:{session,reod:"user session"}
+  //         } 
+  //     }
+  // }
     return {
         props:{
-            user:session
+            user:{}
         }
         
     }
