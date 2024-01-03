7a85627d81632e3aa9f44c555ff7681f33544336
CPCSTL <65110819+CPCSTL@users.noreply.github.com>
2024-01-03 04:36:15 -0600

    finding the bad apple icon

--- components/adminPages/signup/AdminSignin.js
+++ components/adminPages/signup/AdminSignin.js
@@ -10,16 +10,17 @@ import * as yup from 'yup'
 import  {useRouter} from 'next/router'
 import { useDispatch, useSelector } from 'react-redux'
 import { registerUser,signInUser } from 'store/actions/user.actions'
-
-
+import { getSession } from 'next-auth/react'
+import { useSession } from "next-auth/react";
+import { getToken } from 'next-auth/jwt'
 import Image from 'next/image'
-import AdminSignUp from './AdminSignUp';
-
+import AdminSignUp from 'components/adminPages/signup/AdminSignUp'
+import { getServerSession } from 'next-auth/next'
 
 
 
 const SignIn = (props)=>{
-
+ console.log(props.starter, "starter at signin");
   
 
     const [formType, setformType] = useState(true)
@@ -60,7 +61,109 @@ const SignIn = (props)=>{
 
     return (
         <div className="container full_vh small top-space ">
-       hello
+        <Box 
+        sx={{
+          display:"flex",
+          flexWrap:"wrap",
+          width:"100%",
+          pt:"10%",
+          gap:3,
+          marginLeft:"auto",
+          marginRight:"auto",
+        }}
+        
+        >
+       
+       
+      
+       
+        
+        <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "100%" },
+          
+          
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={formik.handleSubmit}
+        >
+        <h1>{formType ? "Sign in":"Register"}</h1>
+        <TextField 
+            name="email"
+            variant='outlined'
+            label='enter your email'
+            {...formik.getFieldProps("email")}
+            {...errorHelper(formik,"email")}
+
+        />
+        <TextField 
+            name="password"
+            variant='outlined'
+            label='enter your password'
+            type='password'
+        
+            {...formik.getFieldProps("password")}
+            {...errorHelper(formik,"password")}
+
+        />
+       <Box
+       sx={{
+          display:"flex",
+          flexDirection:"row",
+          justifyContent:"flex-start",
+          alignItems:"center",
+          width:"100%",
+          marginTop:"10px",
+        
+       }}
+       >
+      
+        <Button 
+        type="submit"
+        variant="contained"
+        color='primary'
+        size="small"
+        className='me-2'
+        disabled={user.loading ? true : false}
+        style={{backgroundColor:"green"}}
+         >
+         {formType ? "Sign in":"Register"}
+        {user.loading ? <CircularProgress size={30} color='secondary' thickness={2} /> :""}
+        </Button>
+        <AdminSignUp open={open} setOpen={setOpen} />
+        </Box>
+
+      
+
+        </Box>
+        <Box sx={{
+          width:{
+            md:"100%",
+            lg:"50%"
+          },
+          height:"20%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+           
+          
+        }} >
+        <Image src="/images/logo.png" alt="logo" width={100} height={100} />
+        <p style={{width:"80%"}} className="text-center">
+      This section of the site is restricted to authorized administrative personnel only. Unauthorized access is strictly prohibited.</p>
+      <Button 
+      variant='contained'
+      onClick={()=>router.push("/")}
+      style={{
+        backgroundColor :"red",
+      }}
+      
+      >Go To HOME PAGE</Button>
+        </Box>
+        </Box>
         </div>
     )
 };
@@ -68,109 +171,31 @@ const SignIn = (props)=>{
 
 
 export default SignIn
-
-
-// <Box 
-// sx={{
-//   display:"flex",
-//   flexWrap:"wrap",
-//   width:"100%",
-//   pt:"10%",
-//   gap:3,
-//   marginLeft:"auto",
-//   marginRight:"auto",
-// }}
-
-// >
-
-
-
-
-
-// <Box
-// component="form"
-// sx={{
-//   "& .MuiTextField-root": { m: 1, width: "100%" },
-  
-  
-// }}
-// noValidate
-// autoComplete="off"
-// onSubmit={formik.handleSubmit}
-// >
-// <h1>{formType ? "Sign in":"Register"}</h1>
-// <TextField 
-//     name="email"
-//     variant='outlined'
-//     label='enter your email'
-//     {...formik.getFieldProps("email")}
-//     {...errorHelper(formik,"email")}
-
-// />
-// <TextField 
-//     name="password"
-//     variant='outlined'
-//     label='enter your password'
-//     type='password'
-
-//     {...formik.getFieldProps("password")}
-//     {...errorHelper(formik,"password")}
-
-// />
-// <Box
-// sx={{
-//   display:"flex",
-//   flexDirection:"row",
-//   justifyContent:"flex-start",
-//   alignItems:"center",
-//   width:"100%",
-//   marginTop:"10px",
-
-// }}
-// >
-
-// <Button 
-// type="submit"
-// variant="contained"
-// color='primary'
-// size="small"
-// className='me-2'
-// disabled={user.loading ? true : false}
-// style={{backgroundColor:"green"}}
-//  >
-//  {formType ? "Sign in":"Register"}
-// {user.loading ? <CircularProgress size={30} color='secondary' thickness={2} /> :""}
-// </Button>
-
-// </Box>
-
-
-
-// </Box>
-// <Box sx={{
-//   width:{
-//     md:"100%",
-//     lg:"50%"
-//   },
-//   height:"20%",
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//     justifyContent: "center",
-   
-  
-// }} >
-// <Image src="/images/logo.png" alt="logo" width={100} height={100} />
-// <p style={{width:"80%"}} className="text-center">
-// This section of the site is restricted to authorized administrative personnel only. Unauthorized access is strictly prohibited.</p>
-// <Button 
-// variant='contained'
-// onClick={()=>router.push("/")}
-// style={{
-// backgroundColor :"red",
-// }}
-
-// >Go To HOME PAGE</Button>
-// </Box>
-// </Box>
-
+// export const getServerSideProps = async (context)=>{
+//     //const session =  await getSession({req:context.req})
+//     // const req = context.req
+//     // let session 
+//   //   try {
+//   //    session = await getSession({req})
+//   //   } catch (error) {
+//   //     console.log(error);
+//   //   }
+//   //   console.log(session , "session at signin");
+//   //   if(session){
+//   //     return {
+//   //         redirect:{
+//   //             destination:"/admins/panel",
+//   //             permanent:false
+//   //         },
+//   //         props:{
+//   //           user:{session,reod:"user session"}
+//   //         } 
+//   //     }
+//   // }
+//     return {
+//         props:{
+//             starter:{adam:"adam miller"}
+//         }
+        
+//     }
+// }
