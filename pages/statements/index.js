e0b1e56f8b980cf57dc3ce5a5cd1e4b086340688
CPCSTL <65110819+CPCSTL@users.noreply.github.com>
2024-01-03 07:36:39 -0600

    found icons in signinup and changed to statcikprops for all statements

--- pages/statements/index.js
+++ pages/statements/index.js
@@ -1,46 +1,40 @@
-import Base from '@layouts/Baseof'
-import LayOutMain from '@layouts/layoutMain/LayOutMain'
-import Box from '@mui/material/Box'
-import AllStatements from 'components/statements/AllStatements'
-import { connectToDb } from 'db/db'
-import {  getAllStatments } from 'db/services/show.service'
-import React from 'react'
-import { toJson } from 'utils/functions'
+import Base from '@layouts/Baseof';
 
-const index = ({statements, testing}) => {
-    console.log(statements,"__statements");
-    console.log(testing,"__testing at statments");
-  return (
-   <Base>
-    <Box>
-    <AllStatements statements={statements} />
-    
-    
-    </Box>
-    </Base>
-   
-  )
+import Box from '@mui/material/Box';
+import AllStatements from 'components/statements/AllStatements';
+import { connectToDb } from 'db/db';
+import { getAllStatments } from 'db/services/show.service';
+import React from 'react';
+import { toJson } from 'utils/functions';
+
+const index = ({ statements, testing }) => {
+    console.log(statements, "__statements");
+    console.log(testing, "__testing at statements");
+    return (
+        <Base>
+            <Box>
+                <AllStatements statements={statements} />
+            </Box>
+        </Base>
+    )
 }
 
-
-
-
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async () => {
     await connectToDb();
-    // const { req } = context;
-
-    // // Check if 'page' query parameter exists and is a valid number, default to 1 if not
-    // const page = parseInt(req.query.page, 10);
-    // const skip = !isNaN(page) && page > 0 ? (page - 1) * 20 : 0;
 
     try {
         const statements = await getAllStatments('_id', 'desc', 20, 0);
-        console.log(statements,"__statements at index");
-        return { props: { statements: toJson(statements),
-        testing:"testing"
-        } };
+        console.log(statements, "__statements at index");
+        return {
+            props: { 
+                statements: toJson(statements),
+                testing: "testing"
+            },
+            revalidate: 60 * 60 * 24, // Revalidate every 24 hours
+        };
     } catch (error) {
         return { props: { statements: [] } };
     }
 };
-export default index
+
+export default index;
