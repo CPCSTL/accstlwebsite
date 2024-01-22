118d7e98e70cdf7b3ed3a267d903e42f61291ea0
CPCSTL <65110819+CPCSTL@users.noreply.github.com>
2024-01-22 15:50:36 -0600

    repair desk script test

--- pages/repair/index.js
+++ pages/repair/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Index = () => {
   return (
     <div>
-    <script id="repairdeskwidget" src="https://phonegeeks.repairdesk.co/widgets/repair_widget.js?token=5dae419862a401571701144&width=100%&scrolling=no" async />
+    <script id="repairdeskwidget" src="https://phonegeeks.repairdesk.co/widgets/repair_widget.js?token=5dae419862a401571701144&width=100%&scrolling=no" defer />
     
     </div>
   )
